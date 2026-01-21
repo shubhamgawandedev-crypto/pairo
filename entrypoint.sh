bb8b8993f17b3d3548a2bd04455ba6efca94dc9f
Shubham <securestud001@gmail.com>
2026-01-21 14:54:52 +0530

    Add entrypoint script for Render

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+exec gunicorn pairo_backend.wsgi:application --bind 0.0.0.0:8000
